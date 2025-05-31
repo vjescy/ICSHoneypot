5d834710816d9f641b3f42998aa6585ecc701c4d
vjescy <vjescy@pm.me>
2025-05-31 19:44:08 +0200

    ScadaBR - working docker-compose, changed network

--- scadaBR/kill_docker.sh
+++ scadaBR/kill_docker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo docker kill $(docker ps -q) && docker rm $(docker ps -a -q) && docker volume rm $(docker volume ls -q) && docker rmi $(docker images -q)
+# Stop and remove all containers
+sudo docker rm -f $(sudo docker ps -aq)
 
+# Remove all images
+sudo docker rmi -f $(sudo docker images -q)
+
+# Remove all user-defined networks (excluding default ones)
+sudo docker network rm $(sudo docker network ls --filter type=custom -q)
